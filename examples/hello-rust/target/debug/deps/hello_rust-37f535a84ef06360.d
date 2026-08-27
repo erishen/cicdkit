a64d35880accc412eacca220fc880eada9aa6f90
Erishen <leisun8309@gmail.com>
2026-08-27 21:46:54 +0800

    chore: 从 git 中移除 target 编译产物目录

--- examples/hello-rust/target/debug/deps/hello_rust-37f535a84ef06360.d
+++
@@ -1,5 +0,0 @@
-/Users/erishen/Workspace/CNB/individular-invest/work/golang/cicdkit/examples/hello-rust/target/debug/deps/hello_rust-37f535a84ef06360.d: src/main.rs
-
-/Users/erishen/Workspace/CNB/individular-invest/work/golang/cicdkit/examples/hello-rust/target/debug/deps/libhello_rust-37f535a84ef06360.rmeta: src/main.rs
-
-src/main.rs:
--- examples/hello-rust/target/debug/deps/hello_rust-81f52297a3654444.d
+++
@@ -1,5 +0,0 @@
-/Users/erishen/Workspace/CNB/individular-invest/work/golang/cicdkit/examples/hello-rust/target/debug/deps/hello_rust-81f52297a3654444.d: src/main.rs
-
-/Users/erishen/Workspace/CNB/individular-invest/work/golang/cicdkit/examples/hello-rust/target/debug/deps/libhello_rust-81f52297a3654444.rmeta: src/main.rs
-
-src/main.rs:
